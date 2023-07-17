9efc638ad88d457c8b80964ec3c2fbd5370ff7ce
Shiny Ballzach <135516150+SporeSun@users.noreply.github.com>
2023-07-16 20:07:16 -0700

    completed for loop, removed extra returns, and fixed typos

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 //defining variables for characters allowed options
 var generatePassword = function() {
-  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var numArray = ['1','2','3','4','5','6','7,','8','9','0'];
-  var specArray = ['!','@','#','$','%','^','&','*','(',')'];
+  var numArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  var specArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
   // WHEN prompted for password criteria
   // THEN I select which criteria to include in the password
   // WHEN prompted for the length of the password
 
-  var passLength = prompt("How long will your password be?")
+  var passLength = prompt("How long will your password be?");
 
   // THEN I choose a length of at least 8 characters and no more than 128 characters
   if (passLength >= 8 && passLength <= 128){
@@ -28,72 +28,58 @@ var generatePassword = function() {
     if(lowerBool && !upperBool && !numBool && !specBool){
       
       finalArray = lowercaseArray;
-      return finalArray;
 
     } else if(upperBool && !lowerBool && !numBool && !specBool){
       
       finalArray = uppercaseArray;
-      return finalArray;
 
     } else if(numBool && !upperBool && !lowerBool && !specBool){
       
       finalArray = numArray;
-      return finalArray;
 
     } else if(specBool && !upperBool && !lowerBool && !numBool){
       
       finalArray = specArray;
-      return finalArray;
 
     } else if (lowerBool && upperBool && !numBool && !specBool){
       
       finalArray = lowercaseArray.concat(uppercaseArray);
-      return finalArray
 
     } else if(lowerBool && numBool && !upperBool && !specBool){
       
       finalArray = lowercaseArray.concat(numArray);
-      return finalArray;
 
     } else if(lowerBool && specBool && !upperBool && numBool){
       
       finalArray = lowercaseArray.concat(specArray);
-      return finalArray;
 
     } else if(upperBool && !lowerBool && numBool && !specBool){
       
       finalArray = uppercaseArray.concat(numArray);
-      return finalArray
 
     } else if(upperBool && !lowerBool && !numBool && specBool){
       
       finalArray = uppercaseArray.concat(specArray);
-      return finalArray;
 
     } else if(!lowerBool && !upperBool && numBool && specBool){
       
       finalArray = numArray.concat(specArray);
-      return finalArray;
 
     } else if(lowerBool && upperBool && numBool && !specBool){
       
       finalArray = lowercaseArray.concat(uppercaseArray, numArray);
-      return finalArray;
 
     } else if(lowerBool && upperBool && !numBool && specBool){
       
       finalArray = lowercaseArray.concat(uppercaseArray, specArray);
-      return finalArray;
 
     } else if(!lowerBool && upperBool && numBool && specBool){
       
       finalArray = uppercaseArray.concat(numArray, specArray);
-      return finalArray;
 
     } else if(lowerBool && upperBool && numBool && specBool){
 
       finalArray = lowercaseArray.concat(uppercaseArray, numArray, specArray);
-      return finalArray;
 
     } else if (!lowerBool && !upperBool && !numBool && !specBool){ //making sure function exits if wrong selections made
       alert("Please select one option for character types");
@@ -102,11 +88,11 @@ var generatePassword = function() {
     //for loop to make the final password
     password = ""
     for (var i = 0; i <= passLength; i++) {
-      var randomNumber = Math.floor(Math.random() * finalArray.length);
+      var finalArrayString = finalArray.toString();
+      var randomNumber = Math.floor(Math.random() * finalArrayString.length);
       password += finalArrayString.substring(randomNumber, randomNumber +1);
-      return finalPassword;
     }
-    return finalPassword;
+    return password;
   } else {
      alert("Password must be at least 8 characters and less than 128 characters");
      return null;
